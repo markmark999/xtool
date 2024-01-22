83307623ed79c481654747a3b4cca55b7a4976cc
Feynman <codingmonkey83@gmail.com>
2024-01-22 14:49:48 +0800

    feat: finished

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
 use std::{
     io::{Read, Write},
-    net::{Ipv4Addr, TcpStream, UdpSocket},
+    net::{Ipv4Addr, TcpStream},
     path::PathBuf,
 };
 
 use clap::{Parser, Subcommand};
 use serialport::SerialPort;
-use tracing_subscriber::{fmt::MakeWriter, prelude::*};
+use tracing_subscriber::prelude::*;
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
 struct Cli {
@@ -29,18 +29,6 @@ struct Cli {
 enum Commands {
     /// does testing for tcp
     Tcp {
-        /// ipv4 address
-        #[arg(short, long)]
-        ip: Ipv4Addr,
-        /// port number
-        #[arg(short, long)]
-        port: u16,
-        /// send raw values (hex)
-        #[arg(short, long, value_parser=from_hex,num_args = 1.., value_delimiter = ' ')]
-        send: Option<Vec<u8>>,
-    },
-    /// does testing for udp
-    Udp {
         /// ipv4 address
         #[arg(short, long)]
         ip: Ipv4Addr,
@@ -74,6 +62,7 @@ trait Transport: Read + Write {}
 impl Transport for TcpStream {}
 
 impl Transport for Box<dyn SerialPort> {}
+
 fn main() -> anyhow::Result<()> {
     tracing_subscriber::registry()
         .with(tracing_subscriber::EnvFilter::from_default_env())
@@ -86,22 +75,36 @@ fn main() -> anyhow::Result<()> {
     let cli = Cli::parse();
 
     let mut args: (Box<dyn Transport>, Option<Vec<u8>>) = match cli.command {
-        Commands::Tcp { ip, port, send } => (
-            Box::new(TcpStream::connect((ip, port))?) as Box<dyn Transport>,
-            send,
-        ),
-        Commands::Udp { ip, port, send } => {
-            todo!()
+        Commands::Tcp { ip, port, send } => {
+            let ts = Box::new(TcpStream::connect((ip, port))?);
+            ts.set_read_timeout(Some(std::time::Duration::from_millis(100)))?;
+            (ts, send)
         }
+
         Commands::Serial {
             port,
             baudrate,
             send,
-        } => (Box::new(serialport::new(port, baudrate).open()?), send),
+        } => {
+            let mut port = Box::new(serialport::new(port, baudrate).open()?);
+
+            port.set_timeout(std::time::Duration::from_millis(500))?;
+            (port, send)
+        }
     };
 
     if let Some(send) = args.1 {
         args.0.write_all(&send)?;
+
+        tracing::info!("sent: {:?}", &send);
     }
+
+    let mut buffer = bytes::BytesMut::with_capacity(1024);
+    loop {
+        if args.0.read(&mut buffer).is_err() {
+            break;
+        }
+    }
+    tracing::info!("received: {:?}", &buffer);
     anyhow::Result::Ok(())
 }
