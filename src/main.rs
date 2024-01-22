511f23e7d5cc337e6c91bc83e7286c0a6016a772
Feynman <codingmonkey83@gmail.com>
2024-01-22 14:55:18 +0800

    feat:  delete options

--- src/main.rs
+++ src/main.rs
@@ -10,17 +10,6 @@ use tracing_subscriber::prelude::*;
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
 struct Cli {
-    /// Optional name to operate on
-    name: Option<String>,
-
-    /// Sets a custom config file
-    #[arg(short, long, value_name = "FILE")]
-    config: Option<PathBuf>,
-
-    /// Turn debugging information on
-    #[arg(short, long, action = clap::ArgAction::Count)]
-    debug: u8,
-
     #[command(subcommand)]
     command: Commands,
 }
